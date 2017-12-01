e4b713ba478ada64d972fc9c78113ecf751649bd
GiangNV <nguyen.van.giang@framgia.com>
2017-12-01 11:01:52 +0700

    Add chat page with Vue example component

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,14 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    /**
+     * Show the chat screen.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function chat()
+    {
+        return view('chat');
+    }
 }
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="chat">
+    <example-component></example-component>
+</div>
+@endsection
